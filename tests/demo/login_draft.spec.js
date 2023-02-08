6b09ee2d4668c1b5dab938a9acff6efd716db1b2
olehpyvovarov <oleg.pyvovarov@gmail.com>
2023-02-08 16:40:44 +0200

    commit1

---
+++ tests/demo/login_draft.spec.js
@@ -0,0 +1,34 @@
+
+
+
+
+ await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.locator('span').filter({ hasText: 'Paul Collings' }).click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+  await page.getByText('Forgot your password?').click();
+  await page.getByText('Please enter your username to identify your account to reset your password').click();
+  await page.getByText('Username', { exact: true }).click();
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByRole('button', { name: 'Cancel' }).click();
+  await page.getByText('Forgot your password?').click();
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByRole('button', { name: 'Reset Password' }).click();
+  await page.getByRole('heading', { name: 'Reset Password link sent successfully' }).click();
+  await page.getByText('A reset password link has been sent to you via email.').click();
+  await page.getByText('You can follow that link and select a new password.').click();
+  await page.getByText('Note:').click();
+  await page.getByText('If the email does not arrive, please contact your OrangeHRM Administrator.').click();
+await page.getByText('Reset Password link sent successfullyA reset password link has been sent to you ').click();
+  
+page.getByRole('ul', { role: 'menu' })
+page.locator('span').filter({ $(span).hasClass(className); })
+page.locator($('span')).hasClass('oxd-userdropdown-name')
+
+});
