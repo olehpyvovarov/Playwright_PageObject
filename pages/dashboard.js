d19fdb5f8dbfc6ce05823e6b9f37bbaf48d2042d
olehpyvovarov <oleg.pyvovarov@gmail.com>
2023-02-09 06:47:44 -0500

    login_logout_2

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,19 +1,22 @@
+//@ts-check
 export class Dashboard {
+    /**
+     * @param {import("playwright-core").Page} page
+     */
     constructor(page) {
         this.page = page;
-        //this.user_dropdown = page.locator($("span")).hasClass("oxd-userdropdown-name");
-        // this.user_dropdown = page
-        //   .locator("span")
-        //   .filter({ hasClass: "--active oxd-userdropdown" });
-        //locator('[class="--active oxd-userdropdown"]');
+       
         this.user_dropdown = page.locator('//span[@class="oxd-userdropdown-tab"]');
         //this.user_menu = page.getByRole("list", { name: "menu" });
-        //this.user_menu = this.user_dropdown.getByRole("listitem");
-        this.user_menu = page.getByText('AboutSupportChange PasswordLogout');
-
+        //this.user_menu = this.user_dropdown.getByRole("list").nth(1);
+        //this.user_menu = page.getByText('AboutSupportChange PasswordLogout');   //this works 1
+        
+        this.user_menu = page.locator('ul .oxd-dropdown-menu'); // this works 2
+        //this.user_menu = page.locator('.oxd-dropdown-menu');  // this works 3
         this.user_logout_button = page
           .getByRole("menuitem", { name: "Logout" });
     }
+
     async logout() {
         await this.user_dropdown.click();
         await this.user_logout_button.click();
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -12,15 +12,18 @@ test('login_test', async ({ page })=>{
       "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
     );
     expect(dashboard.user_dropdown).toBeDefined();
-    await dashboard.user_dropdown.click();
-    
-    console.log(`from dashboard ${dashboard.user_menu}`);
+    await dashboard.user_dropdown.click();        //necessary to assert 'user_menu' and others
+    //await page.pause();
+    //console.log(`LOG from dashboard ${dashboard.user_menu.innerHTML}`); // for debugging - little help
+    await expect(dashboard.user_menu).toBeVisible();
     await expect(dashboard.user_logout_button).toBeVisible();
-    
+
+    console.log(`LOG2 ${await dashboard.user_menu.textContent()}`);  // for debugging - shows text in all elements
+    console.log(`LOG3 ${await dashboard.user_menu.innerText()}`);   // for debugging - shows only text in the 1st element
     await expect(dashboard.user_menu).toContainText('Change Password');
     await expect(dashboard.user_menu.getByRole('listitem')).toHaveCount(4);
 })
-test('logout_test', async ({ page }) => {
+test.skip('logout_test', async ({ page }) => {
     const login = new LoginPage(page);
     const dashboard = new Dashboard(page);
     await login.gotoLoginPage();
